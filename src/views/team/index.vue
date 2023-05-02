fd9e3b6d2dfd8f9c4f12de7fa83b367b8dec5ff8
admin <1119580082@qq.com>
2023-05-03 00:34:57 +0800

    表格添加活水颜色控制

--- src/views/team/index.vue
+++ src/views/team/index.vue
@@ -3,11 +3,11 @@
     <!--搜索组件-->
     <div class="search-box">
       <!--input输入搜索框-->
-      <SearchInput title="团队名" :value.sync="searchData.团队名" @handleSearch="handleSearch"/>
+      <SearchInput title="团队名" :value.sync="searchData.团队名" @handleSearch="handleSearch" />
       <!--select下拉搜索框-->
-      <SearchSelect title="完成比" :value.sync="searchData.完成比" :options="percentOptions" @handleSearch="handleSearch"/>
+      <SearchSelect title="完成比" :value.sync="searchData.完成比" :options="percentOptions" @handleSearch="handleSearch" />
       <!--时间选择器搜索框，时间选择器需要调接口，事件不同为getData-->
-      <SearchDate title="统计时间" :value.sync="searchData.统计时间" @handleSearch="getData"/>
+      <SearchDate title="统计时间" :value.sync="searchData.统计时间" @handleSearch="getData" />
     </div>
 
     <!--tabs选择组件，不需要可删除 tabActiveName：tabs选中项（必填）-->
@@ -26,7 +26,7 @@
           :data="tableShowData"
           :col="col1"
           :default-sort="{prop: 'billing排名', order: 'ascending'}"
-          :loadingShow="loading"
+          :loading-show="loading"
           :max-height="maxHeight"
           @row-click="handleClick"
         >
@@ -53,7 +53,7 @@
           :data="tableShowData"
           :col="col2"
           :default-sort="{prop: '开票完成率排名', order: 'ascending'}"
-          :loadingShow="loading"
+          :loading-show="loading"
           :max-height="maxHeight"
           @row-click="handleClick"
         >
@@ -79,7 +79,7 @@
           :data="tableShowData"
           :col="col3"
           :default-sort="{prop: '到款完成率排名', order: 'ascending'}"
-          :loadingShow="loading"
+          :loading-show="loading"
           :max-height="maxHeight"
           @row-click="handleClick"
         >
@@ -109,7 +109,12 @@
         <el-button size="small" type="primary" style="margin-left: 20px;" @click="fileDownload">下载</el-button>
       </template>
       <!--弹窗表格主体，使用通用表格-->
-      <CommonTable ref="tableData" :data="detailData" :col="detailCol" :max-height="maxHeight"/>
+      <CommonTable ref="tableData" :data="detailData" :col="detailCol" :max-height="maxHeight">
+        <template v-slot:顾问id="{ scope }">
+          <span v-if="scope.row.顾问id.toString().indexOf('(活水)')!==-1" style="color:red;">{{ scope.row.顾问id }}</span>
+          <span v-else>{{ scope.row.顾问id }}</span>
+        </template>
+      </CommonTable>
     </el-dialog>
   </div>
 </template>
@@ -119,11 +124,11 @@ import CommonTable from '@/components/commonTable'
 import SearchInput from '@/components/SearchInput'
 import SearchSelect from '@/components/SearchSelect'
 import SearchDate from '@/components/SearchDate'
-import {getTeamList, achievementdetail} from '@/api/team'
-import {exportToExcel} from '@/utils/tableToExcel'
+import { getTeamList, achievementdetail } from '@/api/team'
+import { exportToExcel } from '@/utils/tableToExcel'
 
 export default {
-  components: {CommonTable, SearchInput, SearchSelect, SearchDate},
+  components: { CommonTable, SearchInput, SearchSelect, SearchDate },
   data() {
     return {
       /* 主体数据，基本页面都需要 */
@@ -131,40 +136,40 @@ export default {
       tableShowData: [], // 表格实际显示数据
       loading: false, // 表格loading判断
       col1: [
-        {label: '一级团队', prop: '一级团队', width: '300'},
-        {label: '全年指标', prop: '全年指标', sortable: true},
-        {label: '当前指标', prop: '当前指标', sortable: true},
-        {label: 'Billing', prop: 'billing', sortable: true},
-        {label: '完成率', prop: 'billing完成率', sortable: true, width: '100'},
-        {label: '完成率排名', prop: 'billing完成率排名', sortable: true},
-        {label: '业绩排名', prop: 'billing排名', sortable: true}
+        { label: '一级团队', prop: '一级团队', width: '300' },
+        { label: '全年指标', prop: '全年指标', sortable: true },
+        { label: '当前指标', prop: '当前指标', sortable: true },
+        { label: 'Billing', prop: 'billing', sortable: true },
+        { label: '完成率', prop: 'billing完成率', sortable: true, width: '100' },
+        { label: '完成率排名', prop: 'billing完成率排名', sortable: true },
+        { label: '业绩排名', prop: 'billing排名', sortable: true }
       ], // 表头数据1
       col2: [
-        {label: '一级团队', prop: '一级团队', width: '300'},
-        {label: '全年指标', prop: '全年指标', sortable: true},
-        {label: '当前指标', prop: '当前指标', sortable: true},
-        {label: '开票', prop: '开票', sortable: true},
-        {label: '完成率', prop: '开票完成率', sortable: true, width: '100'},
-        {label: '完成率排名', prop: '开票排名', sortable: true},
-        {label: '业绩排名', prop: '开票完成率排名', sortable: true}
+        { label: '一级团队', prop: '一级团队', width: '300' },
+        { label: '全年指标', prop: '全年指标', sortable: true },
+        { label: '当前指标', prop: '当前指标', sortable: true },
+        { label: '开票', prop: '开票', sortable: true },
+        { label: '完成率', prop: '开票完成率', sortable: true, width: '100' },
+        { label: '完成率排名', prop: '开票排名', sortable: true },
+        { label: '业绩排名', prop: '开票完成率排名', sortable: true }
       ], // 表头数据2
       col3: [
-        {label: '一级团队', prop: '一级团队', width: '300'},
-        {label: '全年指标', prop: '全年指标', sortable: true},
-        {label: '当前指标', prop: '当前指标', sortable: true},
-        {label: '到款', prop: '到款', sortable: true},
-        {label: '完成率', prop: '到款完成率', sortable: true, width: '100'},
-        {label: '完成率排名', prop: '到款排名', sortable: true},
-        {label: '业绩排名', prop: '到款完成率排名', sortable: true}
+        { label: '一级团队', prop: '一级团队', width: '300' },
+        { label: '全年指标', prop: '全年指标', sortable: true },
+        { label: '当前指标', prop: '当前指标', sortable: true },
+        { label: '到款', prop: '到款', sortable: true },
+        { label: '完成率', prop: '到款完成率', sortable: true, width: '100' },
+        { label: '完成率排名', prop: '到款排名', sortable: true },
+        { label: '业绩排名', prop: '到款完成率排名', sortable: true }
       ], // 表头数据3
 
       /* 搜索组件数据，根据页面进行修改 */
-      searchData: {团队名: '', 完成比: '所有', 统计时间: []}, // 搜索数据
+      searchData: { 团队名: '', 完成比: '所有', 统计时间: [] }, // 搜索数据
       percentOptions: [
-        {label: '所有', value: '所有'},
-        {label: '0-50%', value: '0-50%'},
-        {label: '50-100%', value: '50-100%'},
-        {label: '100%', value: '100%'}
+        { label: '所有', value: '所有' },
+        { label: '0-50%', value: '0-50%' },
+        { label: '50-100%', value: '50-100%' },
+        { label: '100%', value: '100%' }
       ], // 百分比搜索下拉列表
 
       /* tabs组件所需数据，无tabs可删除 */
@@ -174,26 +179,26 @@ export default {
       dialogTableVisible: false, // 控制弹窗显示
       detailData: [], // 弹窗数据
       detailCol: [
-        {label: '发票id', prop: '发票id'},
-        {label: '顾问id', prop: '顾问id'},
-        {label: '顾问名', prop: '顾问名'},
-        {label: '目前团队', prop: '目前团队'},
-        {label: '分配金额', prop: '分配金额'},
-        {label: '添加日期', prop: '添加日期', width: '180'},
-        {label: '核算开票时间', prop: '核算开票时间', width: '180'},
-        {label: '核算到款时间', prop: '核算到款时间', width: '180'}
+        { label: '发票id', prop: '发票id' },
+        { label: '顾问id', prop: '顾问id' },
+        { label: '顾问名', prop: '顾问名' },
+        { label: '目前团队', prop: '目前团队' },
+        { label: '分配金额', prop: '分配金额' },
+        { label: '添加日期', prop: '添加日期', width: '180' },
+        { label: '核算开票时间', prop: '核算开票时间', width: '180' },
+        { label: '核算到款时间', prop: '核算到款时间', width: '180' }
       ] // 弹窗表头数据
     }
-  },
-  mounted() {
-    /* 初始化获取数据 */
-    this.getData()
   },
   computed: {
     /* 表格根据页面自适应高度 */
     maxHeight() {
       return window.innerHeight - 300
     }
+  },
+  mounted() {
+    /* 初始化获取数据 */
+    this.getData()
   },
   methods: {
     // 主表格数据
@@ -204,7 +209,7 @@ export default {
       /* 将搜索框内容保存到vuex中，给页面下载使用 */
       this.$store.dispatch('common/setSearchData', this.searchData)
       /* 请求页面接口数据 */
-      getTeamList({start: this.searchData.统计时间[0], end: this.searchData.统计时间[1], key: localStorage.getItem('key')}).then(res => {
+      getTeamList({ start: this.searchData.统计时间[0], end: this.searchData.统计时间[1], key: localStorage.getItem('key') }).then(res => {
         /* 去除第一列数据 */
         const title = res.data.splice(0, 1)[0]
         /* 处理数据为键值对格式 */
@@ -253,8 +258,8 @@ export default {
       this.detailData = []
       /* 获取表格数据 */
       achievementdetail({
-        start: '2023-04-01',
-        end: '2023-04-30',
+        start: this.searchData.统计时间[0],
+        end: this.searchData.统计时间[1],
         key: 'ahushuai',
         id: row.主团队id,
         type: 1
@@ -262,6 +267,18 @@ export default {
         /* 显示弹窗 */
         this.dialogTableVisible = true
         // 手动处理接口数据改为键值对格式
+        if (res.data1) {
+          res.data1.forEach(item => {
+            const tempObj = {}
+            item.forEach((i, index) => {
+              if (this.detailCol[index].prop === '顾问id') {
+                i = i + '(活水)'
+              }
+              tempObj[this.detailCol[index].prop] = i !== 'None' ? i : ''
+            })
+            this.detailData.push(tempObj)
+          })
+        }
         res.data.forEach(item => {
           const tempObj = {}
           item.forEach((i, index) => {
@@ -274,7 +291,7 @@ export default {
     // 弹窗文件下载(无弹窗可删除)
     fileDownload() {
       exportToExcel(this.detailData, 'myTable', this.detailCol)
-    },
+    }
   }
 }
 </script>
